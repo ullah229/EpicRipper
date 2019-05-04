b86210565d300dd0304a34883f527c33f29ae8d6
Neman Ullah <47436847+ullah229@users.noreply.github.com>
2019-05-04 22:36:15 +0200

    Add files via upload

---
+++ EpicRipper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace EpicRipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                foreach (Process p in Process.GetProcesses())
+                {
+                    string name = p.ProcessName.ToLower();
+                    if (name == "epicgameslauncher") p.Kill();
+                }
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
+
